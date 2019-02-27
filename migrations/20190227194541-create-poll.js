870fae2aace4fac8166601d3fdb1c47534f59c37
FX-Wood <fxwood2@gmail.com>
2019-02-27 11:50:35 -0800

    migrating model and fixing config

--- migrations/20190227194541-create-poll.js
+++ migrations/20190227194541-create-poll.js
@@ -4,9 +4,9 @@ module.exports = {
     return queryInterface.createTable('polls', {
       id: {
         allowNull: false,
-        autoIncrement: true,
+        autoIncrement: false,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.UUID
       },
       name: {
         type: Sequelize.STRING
--- models/poll.js
+++ models/poll.js
@@ -1,10 +1,17 @@
 'use strict';
+const uuid = require('uuid/v4')
 module.exports = (sequelize, DataTypes) => {
   const poll = sequelize.define('poll', {
     name: DataTypes.STRING,
     answer: DataTypes.STRING,
     survey: DataTypes.INTEGER
-  }, {});
+  }, {
+    hooks: {
+      beforeCreate: function(poll) {
+        poll.id = uuid()
+      }
+    }
+  });
   poll.associate = function(models) {
     // associations can be defined here
   };
